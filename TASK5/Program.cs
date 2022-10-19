90ee6273f588f93ef5b2b7dff60a4418c0af4cbd
Vera Lobova <pchilli@yandex.ru>
2022-10-19 13:04:09 +0300

    commit

--- TASK5/Program.cs
+++ TASK5/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//   Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+void Main()
+{
+
+    int [] ourArray = {1,2,3,4,5};
+    int [] copiedArray = GetArrCopy(ourArray);
+   PrintArray(copiedArray);
+}
+
+
+
+int[] GetArrCopy (int[] arr)
+{
+    int[] newArray = new int[arr.Length];
+    for (int i= 0; i< arr.Length; i++)
+    {
+        newArray [i] = arr [i];
+    }
+    return newArray;
+
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("\nНаш новый массив");
+    for ( int i = 0; i<arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} ");
+    }
+}
+Main();
+
